9ef947129f9b044d3f63be006102497b5b45afb0
Angel Angeles <angel.angeles.molina@gmail.com>
2024-05-14 08:34:17 -0600

    Setup Rv

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.stores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridLayout: GridLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.btnSave.setOnClickListener {
+            val store = Store(name = mBinding.etName.text.toString().trim())
+            mAdapter.add(store)
+        }
+
+        setupRv()
+    }
+
+    private fun setupRv() {
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridLayout = GridLayoutManager(this, 2)
+        mBinding.rv.apply {
+            setHasFixedSize(true)
+            adapter = mAdapter
+            layoutManager = mGridLayout
+        }
+
+    }
+
+    override fun onClick(store: Store) {
+        TODO("Not yet implemented")
     }
 }
